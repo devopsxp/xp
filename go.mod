d02fcb78df5f0796ffa8c7297ba3eb517af7baec
Gogs <gogs@fake.local>
2020-12-09 01:11:11 +0800

    finish k8s pod create stage init

--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/briandowns/spinner v1.11.1
 	github.com/google/gops v0.3.13
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
+	github.com/lflxp/lflxp-kubectl v0.0.0-20200324075201-2c87c15ee42c
+	github.com/mattbaird/jsonpatch v0.0.0-20200820163806-098863c1fc24
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/sftp v1.12.0
 	github.com/satori/go.uuid v1.2.0
@@ -19,5 +22,15 @@ require (
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
-	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v2 v2.3.0
+	k8s.io/api v0.0.0-20201114085527-4a626d306b98
+	k8s.io/apimachinery v0.0.0-20201118005411-2456ebdaba22
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.0.0-20201114085527-4a626d306b98
+	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20201118005411-2456ebdaba22
 )
--- roles/k8s_role.go
+++ roles/k8s_role.go
@@ -1,1 +1,155 @@
 package roles
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/devopsxp/xp/pkg/k8s"
+	"github.com/devopsxp/xp/utils"
+	log "github.com/sirupsen/logrus"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func init() {
+	// 初始化k8s role插件映射关系表
+	addRoles(K8sType, reflect.TypeOf(K8sRole{}))
+}
+
+// 对标pod内containerd信息
+type K8sBasic struct {
+	command []string          // 执行命令
+	env     map[string]string // 环境变量
+	args    []string          // 命令参数
+	name    string
+	image   string
+}
+
+type GitRepo struct {
+	url  string
+	user string
+	pwd  string
+}
+
+type K8sRole struct {
+	RoleLC
+	k8s       []K8sBasic // 容器pod组成
+	workspace string     // 共享目录空间
+	repo      GitRepo    // git代码
+	name      string
+}
+
+func (k *K8sRole) Init(args *RoleArgs) error {
+	err := k.Common(args)
+	if err != nil {
+		return err
+	}
+
+	// TODO: fixit
+	k.name = "i don't know"
+	k.workspace = args.workdir
+	k.repo.url = args.reponame
+
+	// TODO: 解析k8s字段
+	tmp := args.currentConfig["k8s"].([]interface{})
+	for _, x := range tmp {
+		k8sbasicData := K8sBasic{
+			command: []string{},
+			env:     map[string]string{},
+			args:    []string{},
+		}
+
+		if n, ok := x.(map[string]interface{})["name"]; ok {
+			if n.(string) == "" {
+				k8sbasicData.name = utils.GetRandomString(32)
+			} else {
+				k8sbasicData.name = n.(string)
+			}
+		}
+
+		if im, ok := x.(map[string]interface{})["image"]; ok {
+			if im.(string) == "" {
+				return errors.New("image is none")
+			} else {
+				k8sbasicData.image = im.(string)
+			}
+		}
+
+		if sc, ok := x.(map[string]interface{})["command"]; ok {
+			for _, it := range sc.([]interface{}) {
+				k8sbasicData.command = append(k8sbasicData.command, it.(string))
+			}
+		}
+
+		if args, ok := x.(map[string]interface{})["args"]; ok {
+			for _, arg := range args.([]interface{}) {
+				k8sbasicData.args = append(k8sbasicData.args, arg.(string))
+			}
+		}
+
+		if e, ok := x.(map[string]interface{})["env"]; ok {
+			for k, v := range e.(map[string]interface{}) {
+				k8sbasicData.env[k] = v.(string)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (k *K8sRole) Run() error {
+	// 组装pod
+	pod := &apiv1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      k.name,
+			Namespace: "default",
+		},
+		Spec: apiv1.PodSpec{
+			Volumes: []apiv1.Volume{
+				apiv1.Volume{
+					Name: "workdir",
+					VolumeSource: apiv1.VolumeSource{
+						EmptyDir: &apiv1.EmptyDirVolumeSource{},
+					},
+				},
+			},
+			Containers: []apiv1.Container{},
+		},
+	}
+
+	// TODO: env from k8srole => EnvVar
+	for _, cc := range k.k8s {
+		containerd := apiv1.Container{
+			Name:    cc.name,
+			Image:   cc.image,
+			Command: cc.command,
+			Args:    cc.args,
+			VolumeMounts: []apiv1.VolumeMount{
+				apiv1.VolumeMount{
+					Name:      "workdir",
+					MountPath: "/workspace",
+				},
+			},
+		}
+		pod.Spec.Containers = append(pod.Spec.Containers, containerd)
+	}
+
+	pod, err := k8s.CreatePod(pod)
+	log.WithFields(log.Fields{
+		"耗时": time.Now().Sub(k.starttime),
+	}).Infof("Pod YAML: %v", pod)
+	return err
+}
+
+// 处理返回日志
+func (k *K8sRole) After() {
+	stoptime := time.Now()
+	k.logs["耗时"] = fmt.Sprintf("%v", stoptime.Sub(k.starttime))
+	k.msg.CallBack[fmt.Sprintf("%s-%s-%s", k.host, k.stage, k.name)] = k.logs
+}
